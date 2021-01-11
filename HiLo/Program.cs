19b80a6acda4fbeb9d854dfad182d1e00c004898
Alex Blackman <alexander.blackman@gmail.com>
2021-01-11 19:54:30 +1100

    finished and tested

--- HiLo/Program.cs
+++ HiLo/Program.cs
@@ -18,6 +18,7 @@ namespace HiLo
             else if (key == '?') HiLoGame.Hint();
             else return;
             }
+            Console.WriteLine("The pot is empty, Bye!");
         }
     }
 
@@ -26,17 +27,46 @@ namespace HiLo
        const int MAXIMUM = 10;
        static Random steve = new Random();
         static int currentNumber = steve.Next(1, MAXIMUM);
-        static private int pot;
+        static private int pot = 10;
 
        public static int GetPot()
         {
             return pot;
         }
 
-         static Guess(bool ishigher)
+    // This method ended up unnecessary, as private variables can be changed internally.
+    //    public static void changePot(int change)
+     //   {
+    //        pot += change;
+     //   }
+
+        public static void Guess(bool ishigher)
         {
-            currentNumber = steve.Next(1, MAXIMUM);
-            return 3;   
+            int newnum = steve.Next(1, MAXIMUM);
+
+            if (((ishigher) && (currentNumber > newnum) || ((!ishigher) && (currentNumber < newnum))))
+            {
+                pot++;
+                Console.WriteLine($"You win a buck; total{GetPot()}\nguess again");
+            }
+            else
+            {
+               pot--;
+                Console.WriteLine($"You lose a whole dollarido, {GetPot()}\nguess again");
+            }
+            Console.WriteLine(currentNumber);
+            currentNumber = newnum;
+
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber >= half)
+                Console.WriteLine($"The number is at least {half}");
+            else
+                Console.WriteLine($"The number is at most {half}");
+            pot--;
         }
 
        
